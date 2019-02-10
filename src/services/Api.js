1b882c9cff3bd4757b7943c37392d8d822751d31
Steven Powell <steven.powell@priceline.com>
2019-02-10 00:40:55 -0600

    testing cloudbuild env vars

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
-
 export default () => {
+    const apiUrl = process.env.CLOUDSDK_COMPUTE_ZONE;
+    console.log(apiUrl);
     return axios.create({
-        baseURL: 'http://localhost:2091'
+        baseURL: apiUrl ? apiUrl : 'http://localhost:2091'
     })
 }
