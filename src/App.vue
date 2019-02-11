04e2de9fb8258eaaa09f3a1ca6f1dec54378bd6c
Ding-X <xiao_peng0202@hotmail.com>
2019-02-10 19:48:27 -0600

    fix merge problem

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,24 @@
 <template>
-  <div class="root"> 
-    <div id="left"> 
-      <Profile/>
-      <!-- <MessList/>  move to iteration2 -->
-      
-    </div> 
-    <div id="right">
+  <div> 
+
       <router-view/>
-    </div> 
+
   </div> 
  
 </template>
 
 <script>
 
-import Profile from "./components/Profile"
-import MessList from "./components/MessList"
 
 export default {
   name: 'App',
   components:{
-    Profile,
-    MessList
   }
 
 }
 </script>
 
 <style>
-body{
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-#left{
-  float: left;
-  width: 300px;
-  height: 100%;
-  overflow: hidden;
-  background: #fafafa;
-  position:fixed;
-  top: 0;
-  left: 0;
-
-}
-#right {
-  color: #2c3e50;
-  float: left;
-  position: relative;overflow: hidden;
-  height: 100%;
-  width: (100%-300px);
-}
 
 
 </style>
