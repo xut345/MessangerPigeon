50281dfefe8555487b6c92030162f16f8242824d
Jay Bali <jaivirbali@live.com>
2019-02-11 07:28:05 -0600

    -Prevent login/register without fields being filled -Fixed register not immediately re-directing to home page once registering -Printed errors to screen as alert for failed login/register attempts

--- src/services/LoginService.js
+++ src/services/LoginService.js
@@ -2,14 +2,24 @@ import Api from '@/services/Api'
 import router from '@/router/index'
 import store from '@/vuex/store'
 
+
 export default {
     register (credentials) {
-        return Api().post('register', credentials).then(function (response) {
-          console.log(response)
-            router.push({ name: 'MessageFrame' })
-          }).catch(function (error) {
-            console.log(error)
-          });
+      return Api().post('register', credentials).then(function (response) {
+        console.log(response)
+        if(response.statusText==='Created'){
+          localStorage.setItem('user', credentials.email);
+          localStorage.setItem('token', response.data.authorization);
+
+          store.commit('login', credentials.email);
+          store.commit('setToken', response.data.authorization);
+          router.push({ name: 'MessageFrame' })
+        } else {
+          alert("An error has occurred with the login process.")
+        }
+      }).catch(function (error) {
+        alert(error)
+      });
     },
     login (credentials) {
         return Api().post('login', credentials)
@@ -23,13 +33,12 @@ export default {
             router.push({ name: 'MessageFrame' })
 
           }
-          else{
-            alert("Your username or password is wrong.")
+          else {
+            alert("An error has occurred with the login process.")
           }
-          
       })     
       .catch(function (error) {
-          console.log(error)
-        });
+        alert(error)
+      });
     }
 }
